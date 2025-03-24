586dce09edd63ca4367ed6e7e220e83391b3d7e1
shashank-179 <shashank17903@gmail.com>
2025-03-24 12:21:45 +0530

    [Shashank Mishra]: ADD: Adding UC4

--- hello-world-app/src/app/app.component.ts
+++ hello-world-app/src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
+  standalone: true, // Standalone component
+  imports: [CommonModule, FormsModule], // Include FormsModule here
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'hello-world-app';
+  userName: string = '';
+  displayName: string = '';
   logoUrl: string = 'assets/logo.png'; // Logo Path
+
+  updateName(event: any) {
+    if (event instanceof KeyboardEvent && event.key === 'Enter') {
+      this.displayName = this.userName; 
+    }
+  }
 
   openBridgeLabzSite() {
     window.open('https://www.bridgelabz.com', '_blank'); // Opens in new tab
